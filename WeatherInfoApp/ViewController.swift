2944f1af84ac612fed013b09cd98c60821024fa7
vinay kumar <vinay.kumar@hubzu.com>
2019-11-21 16:26:17 +0530

    Initial Commit

---
+++ WeatherInfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherInfoApp
+//
+//  Created by Vinay Kumar on 21/11/19.
+//  Copyright © 2019 Vinay Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
